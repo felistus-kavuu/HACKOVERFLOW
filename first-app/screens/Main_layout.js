8c62cf263af736a39017ccc59287b2bc6c681148
briankiptoo <48515925+briankiptoo@users.noreply.github.com>
2022-03-30 15:02:23 +0300

    update

---
+++ first-app/screens/Main_layout.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+    view,
+    text,
+    Home,
+    Profile,
+    Search
+
+} from "screens";
+
+import {COLORS, SIZES, FONTS, CONSTANTS } from "constants";
+
+
+Const MainLayout ={} =>{
+     return{
+         <view
+         style={
+             flex=1,
+             backgroundColor: COLORS.white
+         }
+         >
+               <Text>MainLayout</Text>
+           </view
+
+     }
+}
+
+export default MainLayout;
